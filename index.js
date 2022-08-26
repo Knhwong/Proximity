79e19d6c86e84d581670175a036dcf463ff8b3e8
Knhwong <47486931+Knhwong@users.noreply.github.com>
2022-08-27 00:38:10 +0100

    MongoDB 1
    Prelim MongoDB code

--- index.js
+++ index.js
@@ -5,25 +5,55 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+const { dbo } = require('./db.js')
+const { MongoClient } = require('mongodb');
+
+const url = "mongodb://localhost:27017";
+const dbName = 'GeolocationalChat'
+const client = new MongoClient(url);
+
+var db;
+var userCollection;
+var messages;
+
+
 
 app.get('/', (req, res) => { res.sendFile(__dirname + '/index.html') })
 
-app.get('/jajarbinks!', (req, res) => { res.send("jarjarbinks!") })
 
 io.on('connection', (socket) => {
     console.log("a user connected!");
+
+
+    socket.on('LongLat', (obj) => {
+        console.log("Longitude is " + obj.longitude);
+        console.log("Latitude is " + obj.latitude);
+
+    })
+
+    socket.on('chat message', async(msg) => {
+        io.emit('chat message', msg.msg);
+        console.log(msg.msg);
+
+        const err = await messages.insertOne({
+            uid: socket.id,
+            message: msg,
+        })
+        console.log("Inserted Message =>", err);
+
+    });
+
 
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
 })
 
-io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg.msg);
-        console.log(msg.long);
-        console.log(msg.lati);
-    });
-});
+server.listen(3000, async() => {
+    await client.connect();
+    console.log("Connected to Database")
+    const db = client.db('GeolocationalChat')
+        //userCollection = db.collection('userCollection')
 
-server.listen(3000, () => { console.log("Listening on Port 3000") });
+    console.log("Listening on Port 3000")
+});
