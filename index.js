b59f868c7b06553f6ece49292bc48f386b52b73d
Knhwong <47486931+Knhwong@users.noreply.github.com>
2022-09-05 23:10:23 +0100

    Subscribe to Origin

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { appendFile } = require('fs/promises');
 const http = require('http');
 const { Server } = require("socket.io")
 const app = express();
@@ -10,11 +11,8 @@ const { db } = require("./dbo.js")
 app.get('/', (req, res) => { res.sendFile(__dirname + '/public/index.html') })
 app.use(express.static(__dirname + '/public'));
 
-var connections = []
 io.on('connection', (socket) => {
     console.log("Socket Connected: " + socket.id);
-    connections.push(socket.id);
-
     socket.on('LongLat', async(obj) => {
         //socket.longitude = obj.longitude;
         //socket.latitude = obj.latitude;
@@ -33,34 +31,61 @@ io.on('connection', (socket) => {
     })
 
     socket.on('connectGeo', async(res) => {
-        console.log("Socket ID is " + socket.id);
-        console.log("Longitude is " + res.long);
-        console.log("Latitude is " + res.lat);
         socket.longitude = res.long;
         socket.latitude = res.lat;
-        console.log('// FINDING NEAREST // \n');
         const user = await db.InsertOrUpdate(socket);
+
+
+
+        // We use an array instead of joining rooms to more easily deal with users moving out of range or disconnecting
+        // nearestSockets will be constantly updating, thus removing them rather than requiring the socket to manually disconnect.
+
         const nearest = await db.FindNearest(user, 100);
-        socket.nearest = nearest;
+
+        await Promise.all(nearest.map(async(user) => {
+            const ss = await io.in(user.id).fetchSockets();
+            ss.forEach(s => {
+                s.join(socket.id)
+                console.log(`${s.id} is connected to ${socket.id}`);
+            })
+        }))
+
+
+
+
+        const selfCheck = await io.in(socket.id).fetchSockets();
+        console.log("Self ID is " + selfCheck[0].id);
+
+        //Apparently forEach dosen't work with running interior asyncs...
+        /*
+        for (const user of nearest) {
+            console.log("Fetching socket " + user.id)
+            const s = await io.in(user.id).fetchSockets();
+            s.join(socket.id)
+            console.log(`${s.id} is connected to ${socket.id}`);
+        }*/
+
+
+
         socket.nearestSockets = nearest.map((user) => { return user.id })
-        console.log(socket.nearestSockets);
-        socket.emit('connectGeo', socket.nearest);
+        console.log(`Nearest Sockets to ${socket.id} are: ${socket.nearestSockets}`);
+
+        // Communicate closest sockets
+        socket.emit('connectGeo', socket.nearestSockets);
     })
 
     socket.on('chat message', async(msg) => {
-        console.log("Emitting Chat Message to");
-        console.log(socket.nearestSockets);
-        io.to(socket.nearestSockets).emit('chat message', msg.msg);
-        //socket.emit('chat message', msg.msg);
+
+        socket.to(socket.id).emit('chat message', msg.msg);
         //io.emit('chat message', msg.msg);
         //await db.InsertMessage(socket, msg.msg, msg.timestamp)
     });
 
 
     socket.on('disconnect', async() => {
-        //connections.pop(socket.id)
+
         await db.Remove(socket)
-        console.log('Socket Disconnected ' + socket.id);
+        console.log('User Disconnected ' + socket.id);
     });
 })
 
